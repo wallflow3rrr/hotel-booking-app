448a9ace15e84330b9b5d83217564047da85c900
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-06-01 22:15:54 +0400

    remove useless router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 // ✅ routes.tsx (правильно)
 import { Routes, Route } from 'react-router-dom';
+import AdminPanel from './pages/AdminPanel';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
 import RoomDetail from './pages/RoomDetail';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/room/:id" element={<RoomDetail />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/admin" element={<AdminPanel />} />
   </Routes>
 );
 
